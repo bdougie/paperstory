211647d1561b8e6b444c8300ef1869a3260b1716
Brianllamar <ilikerobot@gmail.com>
2014-03-01 19:19:24 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Paperstory::Application.config.secret_key_base = '3c8406f54e6977f0b123ea3c2dae8c13dfc701f774fdb3a1c9d6fe66f63bc4c0472f95d4d643bd776de9a027482135e5bc7ea7d7e84212360cb7607c75f1d1a5'
