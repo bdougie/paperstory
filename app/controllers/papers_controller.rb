f17841e140c79e06939d67a90b778d004fee213b
Brianllamar <ilikerobot@gmail.com>
2014-03-01 20:01:50 -0500

    paper button and controller complete

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -1,13 +1,40 @@
 class PapersController < ApplicationController
   def index
+  	@paper = Paper.all
   end
 
   def show
+    @church = Paper.find(params[:id])
   end
 
   def new
+  	@paper = Paper.new
   end
 
   def edit
+    @church = Paper.find(params[:id])
   end
-end
+
+  def create
+  	 @paper = Paper.new(paper_params)
+     @paper.save
+   if @paper.save   
+     flash[:notice] = "Paper was saved successfully."
+     redirect_to @paper
+   else
+     flash[:error] = "Error creating paper. Please try again."
+     render new
+    end
+  end
+
+
+
+ private
+
+  # Strong Parameters
+   def paper_params
+     params.require(:paper).permit(:title, :body)
+   end
+
+   
+ end
